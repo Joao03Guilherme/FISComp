a41057f0585fb5b105005c4057c665ebbf4d7a87
João Guilherme <joaommguilherme@tecnico.ulisboa.pt>
2023-05-08 14:44:37 +0100

    final version

--- homework/rImagem.cpp
+++ homework/rImagem.cpp
@@ -7,30 +7,30 @@
 
 using namespace std;
 
-const string INVERTED_IMAGE_FILENAME = "glassware_noisy_inverted.ascii.pgm";
+const string INVERTED_IMAGE_FILENAME = "glassware_noisy_inverted.ascii.pgm"; 
 const string REDUCED_NOISE_IMAGE_FILENAME = "glassware_reduced_noise.ascii.pgm";
 const string BOX_FILTERED_IMAGE_FILENAME = "glassware_box_reduced_noise.ascii.pgm";
 
 int N, width, height;
 string protocol, line;
 
-vector<vector<int>> read_image(string filename);
-vector<int> histogram_absolute(vector<vector<int>> image);
-vector<double> histogram_relative(vector<vector<int>> image);
-void print_image(vector<vector<int>> image);
-template<typename T> void print_histogram(vector<T> const &histogram);
-int average_color(vector<vector<int>> image);
-double variance(vector<vector<int>> image);
-void create_image_file(vector<vector<int>> image, string filename);
-int calculate_eight_average(vector<vector<int>> image, int i, int j);
-vector<vector<int>> create_inverted_image(vector<vector<int>> image);
-vector<vector<int>> create_reduced_noise_image(vector<vector<int>> image);
-int calculate_filtered_average(vector<vector<int>> image, vector<vector<double>> filter, int i, int j);
-vector<vector<int>> create_box_filtering_image(vector<vector<int>> image);
+vector<vector<int>> read_image(string filename); // Read image
+vector<int> histogram_absolute(vector<vector<int>> image); // Create absolute histogram
+vector<double> histogram_relative(vector<vector<int>> image); // Create relative histogram
+void print_image(vector<vector<int>> image); // Print image
+template<typename T> void print_histogram(vector<T> const &histogram); // Print histogram
+int average_color(vector<vector<int>> image); // Calculate average color
+double variance(vector<vector<int>> image); // Calculate variance
+void create_image_file(vector<vector<int>> image, string filename); // Create image file
+int calculate_eight_average(vector<vector<int>> image, int i, int j); // Calculate eight average (8 pixels)
+vector<vector<int>> create_inverted_image(vector<vector<int>> image); // Create inverted image
+vector<vector<int>> create_reduced_noise_image(vector<vector<int>> image); // Create reduced noise image
+int calculate_filtered_average(vector<vector<int>> image, vector<vector<double>> filter, int i, int j); // Calculate filtered average (9 pixels)
+vector<vector<int>> create_box_filtering_image(vector<vector<int>> image); // Create box filtering image
 
 int main(void) {
     vector<vector<int>> image = read_image("glassware_noisy.ascii.pgm"); // Read image
-    vector<vector<int>> inverted_image = create_inverted_image(image);
+    vector<vector<int>> inverted_image = create_inverted_image(image); 
     vector<vector<int>> reduced_noise_image = create_reduced_noise_image(image);
     vector<vector<int>> box_filtered_image = create_box_filtering_image(image);
     return 0;
@@ -114,13 +114,13 @@ int average_color(vector<vector<int>> image) {
     return (int) sum / total;
 }
 
-double variance(vector<vector<int>> image) {
-    int average = average_color(image);
-    double total = width * height * 1.0;
+double variance(vector<vector<int>> image) { 
+    int average = average_color(image); 
+    double total = width * height * 1.0; 
     double sum = 0;
     vector<int> histogram = histogram_absolute(image);
 
-    for (int i = 0; i <= N; i++) {
+    for (int i = 0; i <= N; i++) { 
         sum += histogram[i] * pow(i - average, 2);
     }
 
@@ -130,10 +130,11 @@ double variance(vector<vector<int>> image) {
 void create_image_file(vector<vector<int>> image, string filename) {
     ofstream FILE(filename);
 
-    FILE << "P2\n"; // Protocol
+    FILE << protocol << "\n"; // Protocol
     FILE << width << " " << height << "\n"; // Width and height
+    FILE << N << "\n"; // Max value
 
-    for (int i = 0; i < height; i++) { //
+    for (int i = 0; i < height; i++) { 
         for (int j = 0; j < width; j++) {
             FILE << image[i][j] << " ";
         }
@@ -157,7 +158,7 @@ vector<vector<int>> create_inverted_image(vector<vector<int>> image) {
 int calculate_eight_average(vector<vector<int>> image, int i, int j) {
     int sum = 0;
 
-    for (int dy = -1; dy <= 1; dy++) {
+    for (int dy = -1; dy <= 1; dy++) { // Calculate sum of 8 pixels
         for (int dx = -1; dx <= 1; dx++) {
             if (i + dy < 0 || i + dy >= height || j + dx < 0 || j + dx >= width) sum += 0;
             else {
@@ -171,7 +172,7 @@ int calculate_eight_average(vector<vector<int>> image, int i, int j) {
 
 vector<vector<int>> create_reduced_noise_image(vector<vector<int>> image) {
     vector<vector<int>> new_image(height, vector<int>(width));
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < height; i++) { // Calculate average of 8 pixels
         for (int j = 0; j < width; j++) {
             new_image[i][j] = calculate_eight_average(image, i, j);
         }
@@ -184,7 +185,7 @@ vector<vector<int>> create_reduced_noise_image(vector<vector<int>> image) {
 int calculate_filtered_average(vector<vector<int>> image, vector<vector<double>> filter, int i, int j) {
     double sum = 0;
 
-    for (int dy = -1; dy <= 1; dy++) {
+    for (int dy = -1; dy <= 1; dy++) { // Calculate sum of 8 pixels
         for (int dx = -1; dx <= 1; dx++) {
             if (i + dy < 0 || i + dy >= height || j + dx < 0 || j + dx >= width) sum += 0;
             else {
@@ -200,7 +201,7 @@ vector<vector<int>> create_box_filtering_image(vector<vector<int>> image) {
     vector<vector<double>> filter(3, vector<double>(3, 0.11111)); // Populate array with 1/9
     vector<vector<int>> new_image(height, vector<int>(width));
 
-    for (int i = 0; i < height; i++) {
+    for (int i = 0; i < height; i++) { // Calculate average of 9 pixels
         for (int j = 0; j < width; j++) {
             new_image[i][j] = calculate_filtered_average(image, filter, i, j);
         }
