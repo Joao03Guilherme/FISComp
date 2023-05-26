2ca37c93dc20061863471e0bcf28d9b76f56780e
João Guilherme <joaommguilherme@tecnico.ulisboa.pt>
2023-05-26 17:36:52 +0100

    testing liveshare

---
+++ Projeto_DEV/teste1.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+vector<double> velocidade(){
+
+    cout << "baza vomitar em conjunto" << endl;
+    cout << "em Pombal" << endl;
+
+
+    
+}
+
+
+
+vector<double> acelaracao(){
+
+    cout << "baza vomitar em conjunto" << endl;
+    cout << "em Pombal" << endl;
+
+    //escreva que nem o caralho
+    //escreva que nem o caralho
+    //escreva que Pombal
+    
